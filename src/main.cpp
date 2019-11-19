2053e9c689dbe3312446a3ee32f4117f0b0fc46f
rjlawton <rjlawton@CEM-SOECAL-30>
2019-11-19 11:10:59 -0900

    Early concept of connect 4 board

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(1100,900 ), "Connect4!");
 	sf::CircleShape shape(50.f);
-	shape.setFillColor(sf::Color::White);
+	shape.setFillColor(sf::Color::Cyan);
 
 	while (window.isOpen())
 	{
@@ -17,8 +17,14 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		window.draw(shape);
+
+		window.clear(sf::Color::Blue);
+		for (int row = 0; row < 7; ++row) {
+			for (int column = 0; column < 6; ++column) {
+				shape.setPosition(100+row * 130.f, 100+column * 130.f);
+				window.draw(shape);
+			}
+		}
 		window.display();
 	}
 
