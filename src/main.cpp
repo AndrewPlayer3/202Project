4d134745df110cec063291aca2092b4c748850de
Jordan Day <vdzday@gmail.com>
2019-11-19 10:41:11 -0900

    Reverted previous changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
 /* Test File: Make sure you can run this. */
-//#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-	//sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	//sf::CircleShape shape(100.f);
-	//shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-	//while (window.isOpen())
-	//{
-	//	sf::Event event;
-	//	while (window.pollEvent(event))
-	//	{
-	//		if (event.type == sf::Event::Closed)
-	//			window.close();
-	//	}
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-	//	window.clear();
-	//	window.draw(shape);
-	//	window.display();
-	//}
-	std::cout << "works";
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 
 	return 0;
 }
